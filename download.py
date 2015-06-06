9a1000eaa750df1bb06f58207fef868ffa568980
Johan Herland <johan@herland.net>
2015-06-06 03:34:16 +0200

    Start moving the core code into a new GarminScraper class

--- download.py
+++ download.py
@@ -24,81 +24,6 @@ import re
 import shutil
 import urllib
 
-BASE_URL = "http://connect.garmin.com/en-US/signin"
-GAUTH = "http://connect.garmin.com/gauth/hostname"
-SSO = "https://sso.garmin.com/sso"
-CSS = "https://static.garmincdn.com/com.garmin.connect/ui/css/gauth-custom-v1.1-min.css"
-REDIRECT = "https://connect.garmin.com/post-auth/login"
-ACTIVITIES = "http://connect.garmin.com/proxy/activity-search-service-1.2/json/activities?start=%s&limit=%s"
-ORIG_ZIP = "https://connect.garmin.com/proxy/download-service/files/activity/%s"
-TCX = "https://connect.garmin.com/proxy/activity-service-1.1/tcx/activity/%s?full=true"
-GPX = "https://connect.garmin.com/proxy/activity-service-1.1/gpx/activity/%s?full=true"
-KML = "https://connect.garmin.com/proxy/activity-service-1.0/kml/activity/%s?full=true"
-SPLITS_CSV = "https://connect.garmin.com/csvExporter/%s.csv"
-
-
-def login(agent, username, password):
-    global BASE_URL, GAUTH, REDIRECT, SSO, CSS
-
-    # First establish contact with Garmin and decipher the local host.
-    page = agent.open(BASE_URL)
-    pattern = "\"\S+sso\.garmin\.com\S+\""
-    script_url = re.search(pattern, page.get_data()).group()[1:-1]
-    agent.open(script_url)
-    hostname_url = agent.open(GAUTH)
-    hostname = json.loads(hostname_url.get_data())['host']
-
-    # Package the full login GET request...
-    data = {
-        'service': REDIRECT,
-        'webhost': hostname,
-        'source': BASE_URL,
-        'redirectAfterAccountLoginUrl': REDIRECT,
-        'redirectAfterAccountCreationUrl': REDIRECT,
-        'gauthHost': SSO,
-        'locale': 'en_US',
-        'id': 'gauth-widget',
-        'cssUrl': CSS,
-        'clientId': 'GarminConnect',
-        'rememberMeShown': 'true',
-        'rememberMeChecked': 'false',
-        'createAccountShown': 'true',
-        'openCreateAccount': 'false',
-        'usernameShown': 'false',
-        'displayNameShown': 'false',
-        'consumeServiceTicket': 'false',
-        'initialFocus': 'true',
-        'embedWidget': 'false',
-        'generateExtraServiceTicket': 'false',
-    }
-
-    # ...and officially say "hello" to Garmin Connect.
-    login_url = 'https://sso.garmin.com/sso/login?%s' % urllib.urlencode(data)
-    agent.open(login_url)
-
-    # Set up the login form.
-    agent.select_form(predicate=lambda f: 'id' in f.attrs and f.attrs['id'] == 'login-form')
-    agent['username'] = username
-    agent['password'] = password
-    agent.addheaders = [('User-agent', 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/535.2 (KHTML, like Gecko) Chrome/15.0.874.121 Safari/535.2'), ]
-    # Apparently Garmin Connect attempts to filter on these browser headers;
-    # without them, the login will fail.
-
-    # Submit the login!
-    res = agent.submit()
-    if res.get_data().find("Invalid") >= 0:
-        quit("Login failed! Check your credentials, or submit a bug report.")
-    elif res.get_data().find("SUCCESS") >= 0:
-        print('Login successful! Proceeding...')
-    else:
-        quit('UNKNOWN STATE. This script may need to be updated. Submit a bug report.')
-
-    # Now we need a very specific URL from the response.
-    response_url = re.search("response_url\s*=\s*'(.*)';", res.get_data()).groups()[0]
-    agent.open(response_url)
-
-    # In theory, we're in.
-
 
 def file_exists_in_folder(filename, folder):
     "Check if the file exists in folder or any subfolder"
@@ -108,49 +33,125 @@ def file_exists_in_folder(filename, folder):
     return False
 
 
-def activities(agent, outdir, increment=100):
-    global ACTIVITIES
-    currentIndex = 0
-    initUrl = ACTIVITIES % (currentIndex, increment)  # 100 activities seems a nice round number
-    try:
-        response = agent.open(initUrl)
-    except:
-        print('Wrong credentials for user {}. Skipping.'.format(username))
-        return
-    search = json.loads(response.get_data())
-    totalActivities = int(search['results']['totalFound'])
-    while True:
-        for item in search['results']['activities']:
-            # Read this list of activities and save the files.
-            activityId = item['activity']['activityId']
-            url = TCX % activityId
-            file_name = '{}_{}.txt'.format(username, activityId)
-            if file_exists_in_folder(file_name, outdir):
-                print('{} already exists in {}. Skipping.'.format(file_name, outdir))
-                continue
-            print('{} is downloading...'.format(file_name))
-            datafile = agent.open(url).get_data()
-            file_path = os.path.join(outdir, file_name)
-            f = open(file_path, "w")
-            f.write(datafile)
-            f.close()
-            shutil.copy(file_path, os.path.join(os.path.dirname(os.path.dirname(file_path)), file_name))
+class GarminScraper(object):
 
-        if (currentIndex + increment) > totalActivities:
-            # All done!
-            break
+    BASE_URL = "http://connect.garmin.com/en-US/signin"
+    GAUTH = "http://connect.garmin.com/gauth/hostname"
+    SSO = "https://sso.garmin.com/sso"
+    CSS = "https://static.garmincdn.com/com.garmin.connect/ui/css/gauth-custom-v1.1-min.css"
+    REDIRECT = "https://connect.garmin.com/post-auth/login"
+    ACTIVITIES = "http://connect.garmin.com/proxy/activity-search-service-1.2/json/activities?start=%s&limit=%s"
+    ORIG_ZIP = "https://connect.garmin.com/proxy/download-service/files/activity/%s"
+    TCX = "https://connect.garmin.com/proxy/activity-service-1.1/tcx/activity/%s?full=true"
+    GPX = "https://connect.garmin.com/proxy/activity-service-1.1/gpx/activity/%s?full=true"
+    KML = "https://connect.garmin.com/proxy/activity-service-1.0/kml/activity/%s?full=true"
+    SPLITS_CSV = "https://connect.garmin.com/csvExporter/%s.csv"
 
-        # We still have at least 1 activity.
-        currentIndex += increment
-        url = ACTIVITIES % (currentIndex, increment)
-        response = agent.open(url)
+    def __init__(self, username):
+        self.username = username
+        self.agent = mechanize.Browser()
+
+    def login(self, password):
+        # First establish contact with Garmin and decipher the local host.
+        page = self.agent.open(self.BASE_URL)
+        pattern = "\"\S+sso\.garmin\.com\S+\""
+        script_url = re.search(pattern, page.get_data()).group()[1:-1]
+        self.agent.open(script_url)
+        hostname_url = self.agent.open(self.GAUTH)
+        hostname = json.loads(hostname_url.get_data())['host']
+
+        # Package the full login GET request...
+        data = {
+            'service': self.REDIRECT,
+            'webhost': hostname,
+            'source': self.BASE_URL,
+            'redirectAfterAccountLoginUrl': self.REDIRECT,
+            'redirectAfterAccountCreationUrl': self.REDIRECT,
+            'gauthHost': self.SSO,
+            'locale': 'en_US',
+            'id': 'gauth-widget',
+            'cssUrl': self.CSS,
+            'clientId': 'GarminConnect',
+            'rememberMeShown': 'true',
+            'rememberMeChecked': 'false',
+            'createAccountShown': 'true',
+            'openCreateAccount': 'false',
+            'usernameShown': 'false',
+            'displayNameShown': 'false',
+            'consumeServiceTicket': 'false',
+            'initialFocus': 'true',
+            'embedWidget': 'false',
+            'generateExtraServiceTicket': 'false',
+        }
+
+        # ...and officially say "hello" to Garmin Connect.
+        login_url = 'https://sso.garmin.com/sso/login?%s' % urllib.urlencode(data)
+        self.agent.open(login_url)
+
+        # Set up the login form.
+        self.agent.select_form(predicate=lambda f: 'id' in f.attrs and f.attrs['id'] == 'login-form')
+        self.agent['username'] = self.username
+        self.agent['password'] = password
+        self.agent.addheaders = [('User-agent', 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/535.2 (KHTML, like Gecko) Chrome/15.0.874.121 Safari/535.2'), ]
+        # Apparently Garmin Connect attempts to filter on these browser headers;
+        # without them, the login will fail.
+
+        # Submit the login!
+        res = self.agent.submit()
+        if res.get_data().find("Invalid") >= 0:
+            quit("Login failed! Check your credentials, or submit a bug report.")
+        elif res.get_data().find("SUCCESS") >= 0:
+            print('Login successful! Proceeding...')
+        else:
+            quit('UNKNOWN STATE. This script may need to be updated. Submit a bug report.')
+
+        # Now we need a very specific URL from the response.
+        response_url = re.search("response_url\s*=\s*'(.*)';", res.get_data()).groups()[0]
+        self.agent.open(response_url)
+
+        # In theory, we're in.
+
+    def activities(self, outdir, increment=100):
+        currentIndex = 0
+        initUrl = self.ACTIVITIES % (currentIndex, increment)  # 100 activities seems a nice round number
+        try:
+            response = self.agent.open(initUrl)
+        except:
+            print('Wrong credentials for user {}. Skipping.'.format(self.username))
+            return
         search = json.loads(response.get_data())
+        totalActivities = int(search['results']['totalFound'])
+        while True:
+            for item in search['results']['activities']:
+                # Read this list of activities and save the files.
+                activityId = item['activity']['activityId']
+                url = self.TCX % activityId
+                file_name = '{}_{}.txt'.format(self.username, activityId)
+                if file_exists_in_folder(file_name, outdir):
+                    print('{} already exists in {}. Skipping.'.format(file_name, outdir))
+                    continue
+                print('{} is downloading...'.format(file_name))
+                datafile = self.agent.open(url).get_data()
+                file_path = os.path.join(outdir, file_name)
+                f = open(file_path, "w")
+                f.write(datafile)
+                f.close()
+                shutil.copy(file_path, os.path.join(os.path.dirname(os.path.dirname(file_path)), file_name))
+
+            if (currentIndex + increment) > totalActivities:
+                # All done!
+                break
+
+            # We still have at least 1 activity.
+            currentIndex += increment
+            url = self.ACTIVITIES % (currentIndex, increment)
+            response = self.agent.open(url)
+            search = json.loads(response.get_data())
 
 
 def download_files_for_user(username, password, output):
-    # Create the agent and log in.
-    agent = mechanize.Browser()
-    login(agent, username, password)
+    gs = GarminScraper(username)
+    gs.login(password)
 
     user_output = os.path.join(output, username)
     download_folder = os.path.join(user_output, 'Historical')
@@ -160,7 +161,7 @@ def download_files_for_user(username, password, output):
         os.makedirs(download_folder)
 
     # Scrape all the activities.
-    activities(agent, download_folder)
+    gs.activities(download_folder)
 
 
 def credentials_from_prompt():
