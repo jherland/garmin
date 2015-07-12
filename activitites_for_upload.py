6655a4f3180d59b9d7aeb6c97b4f4bf4fece0bf8
Johan Herland <johan@herland.net>
2015-07-12 02:54:34 +0200

    Add module for listing activities that are either on Garmin or on Strava

---
+++ activitites_for_upload.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python2
+
+from __future__ import print_function
+
+import os
+
+import garmin
+import strava
+
+
+def main():
+    import argparse
+
+    parser = argparse.ArgumentParser(description='Strava Activity lister')
+    parser.add_argument(
+        '-g', '--garmin', default='./garmin',
+        help='Directory where Garmin activities are stored.')
+    parser.add_argument(
+        '-s', '--strava', default='./strava',
+        help='Directory where Strava activities (.gpx files) are stored.')
+    args = parser.parse_args()
+
+    s_acts = list(strava.walk_activities(args.strava, sorted=True))
+    print('Found {} Strava activities in {}'.format(len(s_acts), args.strava))
+    g_store = garmin.GarminStore(args.garmin)
+    g_acts = list(g_store.walk(sorted=True))
+    print('Found {} Garmin activities in {}'.format(len(g_acts), args.garmin))
+
+    g_only, both, s_only = [], [], []
+    while g_acts and s_acts:
+        if g_acts[0].when < s_acts[0].when:
+            g_only.append(g_acts.pop(0))
+        elif g_acts[0].when == s_acts[0].when:
+            both.append((g_acts.pop(0), s_acts.pop(0)))
+        else:  # g_acts[0].when > s_acts[0].when
+            s_only.append(s_acts.pop(0))
+
+    if both:
+        print('Present on both Garmin and Strava ({}):'.format(len(both)))
+        for g, s in both:
+            print('   ', g, '/', s)
+    if g_only:
+        print('Only on Garmin ({}):'.format(len(g_only)))
+        for g in g_only:
+            print('   ', g)
+    if s_only:
+        print('Only on Strava ({}):'.format(len(s_only)))
+        for s in s_only:
+            print('   ', s)
+
+
+if __name__ == '__main__':
+    main()
